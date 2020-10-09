727258f0e64f8a6ff11910e6b1147d3db9aada79
Luke <geyserlg@gmail.com>
2020-10-09 11:03:59 +0200

    Added Project

---
+++ ATS/ATS/Forms/MainWindow.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ATS
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnl_Inputs = new System.Windows.Forms.Panel();
+            this.pnl_Inputs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inputs:";
+            // 
+            // pnl_Inputs
+            // 
+            this.pnl_Inputs.Controls.Add(this.label1);
+            this.pnl_Inputs.Location = new System.Drawing.Point(12, 12);
+            this.pnl_Inputs.Name = "pnl_Inputs";
+            this.pnl_Inputs.Size = new System.Drawing.Size(989, 242);
+            this.pnl_Inputs.TabIndex = 1;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1013, 671);
+            this.Controls.Add(this.pnl_Inputs);
+            this.Name = "MainWindow";
+            this.Text = "Arbitrade Trading Software";
+            this.pnl_Inputs.ResumeLayout(false);
+            this.pnl_Inputs.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnl_Inputs;
+    }
+}
+
