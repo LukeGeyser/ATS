eb0af037aaf318a74d6eb0180d5dbaa093b1a72f
Luke <geyserlg@gmail.com>
2020-10-15 23:27:35 +0200

    Added Valr Service and included RestSharp to the project

--- ATS/ATS/Helpers/ValrValidator.cs
+++ ATS/ATS/Helpers/ValrValidator.cs
@@ -4,7 +4,7 @@ using System.Security.Cryptography;
 
 namespace ATS.Helpers
 {
-    public class ValrValidator
+    public static class ValrValidator
     {
         public static string SignInRequest(string ApiKeySecret, string TimeStamp, string Verb, string Path, string Body = "")
         {
@@ -12,11 +12,33 @@ namespace ATS.Helpers
 
             byte[] payload = Encoding.UTF8.GetBytes(TempPayload);
 
-            using (HMACSHA512 hmac = new HMACSHA512(Encoding.UTF8.GetBytes(ApiKeySecret))
+            using (HMACSHA512 hmac = new HMACSHA512(Encoding.UTF8.GetBytes(ApiKeySecret)))
             {
-                //byte[] hash = hmac.ComputeHash()
-            })
-            return null;
+                byte[] hash = hmac.ComputeHash(payload);
+                return ConvertToHexString(hash);
+            }
+        }
+
+        private static string ConvertToHexString(byte[] hash)
+        {
+            StringBuilder result = new StringBuilder(hash.Length * 2);
+
+            foreach (var c in hash)
+            {
+                result.Append(c.ToString("x2"));
+            }
+
+            return result.ToString();
+        }
+
+        /* Time-stamp in milliseconds. 
+         * The same time-stamp should be used to generate request signature
+         * as well as sent along in the X-VALR-TIMESTAMP header of the request
+         */
+
+        private static string GetTimeStamp()
+        {
+            return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds().ToString();
         }
     }
 }
---
+++ ATS/ATS/Services/ValrService.cs
@@ -0,0 +1,20 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATS.Services
+{
+    public static class ValrService
+    {
+
+        public static void Test(string ApiKeySecret, string TimeStamp, string Verb, string Path, string Body = "")
+        {
+            //RestRequest
+        }
+
+    }
+}
