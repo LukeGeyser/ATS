297562566e01f606e31c456b0ccd5ad55a91054d
Luke <geyserlg@gmail.com>
2020-10-19 11:56:24 +0200

    Push

--- ATS/ATS/Forms/MainWindow.cs
+++ ATS/ATS/Forms/MainWindow.cs
@@ -1,8 +1,10 @@
 ﻿using ATS.Services;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -25,7 +27,11 @@ namespace ATS
 
         private void btn_ActivateMonitoring_Click(object sender, EventArgs e)
         {
-            ValrService.Test("", "", "GET", "account/balances");
+            IRestResponse response = ValrService.Get("881c6b0beb12baa68875b2c51bc82b5cfb175bfe3492cfdbe7af045afaf765f3", "99a5c2c177959c19856546b789cdc715737c50d0a6af61abc6a88e9428f50933", "GET", "/v1/account/balances");
+
+            if (!response.IsSuccessful)
+                Debug.WriteLine($"ERROR: {response.Content}");
+
         }
     }
 }
--- ATS/ATS/Services/ValrService.cs
+++ ATS/ATS/Services/ValrService.cs
@@ -13,14 +13,26 @@ namespace ATS.Services
     public static class ValrService
     {
 
-        public static void Test(string ApiKey, string ApiKeySecret, string Verb, string Path, string Body = "")
+        public static IRestResponse Get(string ApiKey, string ApiKeySecret, string Verb, string Path, string Body = "")
         {
             var _timeStamp = ValrValidator.GetTimeStamp();
             string rqstSig;
+            IRestResponse response;
 
             try
             {
                 rqstSig = ValrValidator.SignInRequest(ApiKeySecret, _timeStamp, Verb, Path, Body);
+
+                var client = new RestClient(string.Format($"https://api.valr.com/{Path}"));
+
+                client.Timeout = -1;
+
+                var request = new RestRequest((Method)Enum.Parse(typeof(Method), Verb));
+                request.AddHeader("X-VALR-API-KEY", ApiKey);
+                request.AddHeader("X-VALR-SIGNATURE", rqstSig);
+                request.AddHeader("X-VALR-TIMESTAMP", _timeStamp);
+
+                response = client.Execute(request);
             }
             catch (Exception)
             {
@@ -28,18 +40,7 @@ namespace ATS.Services
                 throw;
             }
 
-            var client = new RestClient(string.Format($"https://api.valr.com/v1/{Path}"));
-
-            client.Timeout = -1;
-            
-            var request = new RestRequest((Method)Enum.Parse(typeof(Method), Verb));
-            request.AddHeader("X-VALR-API-KEY", ApiKey);
-            request.AddHeader("X-VALR-SIGNATURE", rqstSig);
-            request.AddHeader("X-VALR-TIMESTAMP", _timeStamp);
-
-            IRestResponse response = client.Execute(request);
-
-            Debug.WriteLine(response.Content);
+            return response;
         }
 
     }
