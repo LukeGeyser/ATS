cd656ca4b06f02dfaa7841f1aaa1e80c002f7f9c
JordanReighn <50360815+JordanReighn@users.noreply.github.com>
2021-02-02 18:45:33 +0200

    Label Update

--- ATS/ATS/Forms/MainWindow.Designer.cs
+++ ATS/ATS/Forms/MainWindow.Designer.cs
@@ -80,6 +80,17 @@
             this.pnl_ErrorLog = new System.Windows.Forms.Panel();
             this.dgv_errorLog = new System.Windows.Forms.DataGridView();
             this.lbl_errorLog = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
             this.mainWindowBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.pnl_Inputs.SuspendLayout();
             this.pnl_monitor.SuspendLayout();
@@ -101,6 +112,10 @@
             // 
             // pnl_Inputs
             // 
+            this.pnl_Inputs.Controls.Add(this.label10);
+            this.pnl_Inputs.Controls.Add(this.label8);
+            this.pnl_Inputs.Controls.Add(this.label7);
+            this.pnl_Inputs.Controls.Add(this.label6);
             this.pnl_Inputs.Controls.Add(this.txtBTL);
             this.pnl_Inputs.Controls.Add(this.label2);
             this.pnl_Inputs.Controls.Add(this.txtBitstampApiKeySecret);
@@ -165,7 +180,7 @@
             // 
             this.lblBTIError.AutoSize = true;
             this.lblBTIError.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.lblBTIError.Location = new System.Drawing.Point(268, 93);
+            this.lblBTIError.Location = new System.Drawing.Point(302, 93);
             this.lblBTIError.Name = "lblBTIError";
             this.lblBTIError.Size = new System.Drawing.Size(120, 13);
             this.lblBTIError.TabIndex = 20;
@@ -310,6 +325,13 @@
             // 
             // pnl_monitor
             // 
+            this.pnl_monitor.Controls.Add(this.label17);
+            this.pnl_monitor.Controls.Add(this.label16);
+            this.pnl_monitor.Controls.Add(this.label15);
+            this.pnl_monitor.Controls.Add(this.label14);
+            this.pnl_monitor.Controls.Add(this.label13);
+            this.pnl_monitor.Controls.Add(this.label12);
+            this.pnl_monitor.Controls.Add(this.label11);
             this.pnl_monitor.Controls.Add(this.txtSellTotal);
             this.pnl_monitor.Controls.Add(this.label4);
             this.pnl_monitor.Controls.Add(this.txtBuyTotal);
@@ -570,6 +592,105 @@
             this.lbl_errorLog.TabIndex = 0;
             this.lbl_errorLog.Text = "Error Log";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(268, 62);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "EUR/ZAR";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(268, 93);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 13);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "BTC";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(268, 125);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(15, 13);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "%";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(268, 156);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(28, 13);
+            this.label10.TabIndex = 28;
+            this.label10.Text = "BTC";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(268, 43);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(29, 13);
+            this.label11.TabIndex = 29;
+            this.label11.Text = "ZAR";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(268, 79);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(30, 13);
+            this.label12.TabIndex = 30;
+            this.label12.Text = "EUR";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(267, 113);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(29, 13);
+            this.label13.TabIndex = 31;
+            this.label13.Text = "ZAR";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(268, 146);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(15, 13);
+            this.label14.TabIndex = 29;
+            this.label14.Text = "%";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(575, 43);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(15, 13);
+            this.label15.TabIndex = 32;
+            this.label15.Text = "%";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(575, 79);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(28, 13);
+            this.label16.TabIndex = 29;
+            this.label16.Text = "BTC";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(575, 113);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(28, 13);
+            this.label17.TabIndex = 33;
+            this.label17.Text = "BTC";
+            // 
             // mainWindowBindingSource
             // 
             this.mainWindowBindingSource.DataSource = typeof(ATS.MainWindow);
@@ -653,6 +774,17 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtBuyTotal;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
     }
 }
 
